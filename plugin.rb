a055cfae08742a4c99c69f79321f9cc559cb66f6
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2018-06-17 14:16:35 +0530

    FEATURE: Add user fields

---
+++ plugin.rb
@@ -0,0 +1,23 @@
+# name: procourse-autoresponder
+# about: Set dates and times for sending automatic responses to Discourse group messages.
+# version: 1.0
+# authors: Procourse
+# url: https://github.com/procourse/procourse-autoresponder
+
+enabled_site_setting :autoresponder_enabled
+
+after_initialize do
+# Add custom user preference for setting
+User.register_custom_field_type('autoresponder_start_date', :date)
+User.register_custom_field_type('autoresponder_start_time', :time)
+User.register_custom_field_type('autoresponder_end_date', :date)
+User.register_custom_field_type('autoresponder_end_time', :time)
+
+public_user_custom_fields = SiteSetting.public_user_custom_fields.split('|')
+public_user_custom_fields.push('autoresponder_start_date')
+public_user_custom_fields.push('autoresponder_start_time')
+public_user_custom_fields.push('autoresponder_end_date')
+public_user_custom_fields.push('autoresponder_end_time')
+SiteSetting.public_user_custom_fields = public_user_custom_fields.join('|')
+
+end
