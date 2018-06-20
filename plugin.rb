b0a437cbb11bd9a6798ab28807a752b315b06a95
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2018-06-20 19:03:55 +0530

    Feature: Complete the user fields

--- plugin.rb
+++ plugin.rb
@@ -9,15 +9,11 @@ enabled_site_setting :autoresponder_enabled
 after_initialize do
 # Add custom user preference for setting
 User.register_custom_field_type('autoresponder_start_date', :date)
-User.register_custom_field_type('autoresponder_start_time', :time)
 User.register_custom_field_type('autoresponder_end_date', :date)
-User.register_custom_field_type('autoresponder_end_time', :time)
 
 public_user_custom_fields = SiteSetting.public_user_custom_fields.split('|')
 public_user_custom_fields.push('autoresponder_start_date')
-public_user_custom_fields.push('autoresponder_start_time')
 public_user_custom_fields.push('autoresponder_end_date')
-public_user_custom_fields.push('autoresponder_end_time')
 SiteSetting.public_user_custom_fields = public_user_custom_fields.join('|')
 
 end
